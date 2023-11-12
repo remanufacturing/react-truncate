9f3c150a8464b42d51935507dfec243fe6707d2b
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-11-12 22:42:29 +0800

    feat(examples): add shared data

---
+++ examples/shared/README.md
@@ -0,0 +1,5 @@
+# shared
+
+This is a shared static resource used by all examples, and usually does not need to be changed.
+
+这是提供给所有示例项目使用的共享静态资源，通常情况下不需要改动它。
---
+++ examples/shared/data.tsx
@@ -0,0 +1,107 @@
+import React from 'react'
+
+export const RichText: React.FC = () => (
+  <>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur{' '}
+      <a
+        href="https://www.yahoo.com/"
+        className="my_css_class"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        yahoo.com
+      </a>{' '}
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam,{' '}
+      <a
+        href="https://www.google.bg/"
+        title="Google"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        www.google.bg
+      </a>{' '}
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum
+      dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore
+    </p>
+
+    <p>
+      et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+    </p>
+
+    <p>
+      ullamco laboris nisi ut aliquip ex Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex Lorem ipsum dolor sit amet, consectetur
+    </p>
+
+    <p>
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit, sed
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur{' '}
+      <a
+        href="https://www.yahoo.com/"
+        className="my_css_class"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        yahoo.com
+      </a>{' '}
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam,{' '}
+      <a
+        href="https://www.google.bg/"
+        title="Google"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        www.google.bg
+      </a>{' '}
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum
+      dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore
+    </p>
+
+    <p>
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex Lorem
+      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip ex
+    </p>
+  </>
+)
