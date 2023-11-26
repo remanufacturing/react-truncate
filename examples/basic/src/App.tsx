6e31cddf9076dc115f0ef5481fe569a912ada985
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-11-26 14:25:18 +0800

    feat(examples): add width controller

--- examples/basic/src/App.tsx
+++ examples/basic/src/App.tsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RichText } from 'shared/data'
 import { ShowMore } from '@re-dev/react-truncate'
 
 const App: React.FC = () => {
+  const [range, setRange] = useState(100)
+
   return (
-    <ShowMore>
-      <RichText />
-    </ShowMore>
+    <div className="container">
+      <p>
+        <a
+          href="https://github.com/remanufacturing/react-truncate/blob/main/examples/basic/src/App.tsx"
+          target="_blank"
+          rel="noreferrer"
+        >
+          🔗 Source code of this example
+        </a>
+      </p>
+
+      <div className="tips">
+        <h3>Controllable Usage:</h3>
+        <p>Defaults to 3 lines and will follow window size changes.</p>
+        <p>
+          Adjust the width of the parent element and it will change accordingly.
+        </p>
+        <p>
+          <input
+            className="range"
+            type="range"
+            min="50"
+            max="100"
+            defaultValue={100}
+            onChange={(e) => setRange(Number(e.target.value))}
+          />
+
+          <span>{range}%</span>
+        </p>
+      </div>
+
+      <div style={{ width: `${range}%` }}>
+        <ShowMore>
+          <RichText />
+        </ShowMore>
+      </div>
+    </div>
   )
 }
 
