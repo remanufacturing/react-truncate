155b70c2e2046ec175a06d73daf0c422e0f72825
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-04-21 01:31:18 +0800

    feat(docs): add ShowMore controllable examples

--- docs/src/components/ControllableShowMore.tsx
+++ docs/src/components/ControllableShowMore.tsx
@@ -1,93 +1,124 @@
-import React, { useState } from 'react'
-import { clsx } from 'clsx'
-import { ShowMore } from '@re-dev/react-truncate'
+import React, { useMemo, useRef, useState } from 'react'
+import {
+  ShowMore,
+  type ShowMoreRef,
+  type ShowMoreToggleLinesFn,
+} from '@re-dev/react-truncate'
 import {
   RichText,
   ChineseRichText,
   ChineseStringText,
   StringText,
 } from './ExampleData'
-import { ExampleContainer } from './ExampleContainer'
-import { getTranslation, type Languages } from '@/i18n'
+import { ExampleContainer, FormRange, FormSwitch } from './ExampleWidgets'
+import { type Languages } from '@/i18n'
+
+const CustomButton: React.FC<{
+  type: 'more' | 'less'
+  isZh: boolean
+  onClick: ShowMoreToggleLinesFn
+}> = ({ type, isZh, onClick }) => {
+  const label = useMemo(() => {
+    if (isZh) {
+      return type === 'more' ? `展开` : '收起'
+    }
+    return type === 'more' ? `Show More` : 'Show Less'
+  }, [type])
+
+  return (
+    <button className="text-xs ml-2 cursor-pointer" onClick={onClick}>
+      {label}
+    </button>
+  )
+}
 
 const CurrentContent: React.FC<{
-  lang: Languages
+  isZh: boolean
   html: boolean
-}> = ({ lang, html }) => {
-  if (lang === 'zh') {
+}> = ({ isZh, html }) => {
+  if (isZh) {
     return html ? <ChineseRichText /> : <ChineseStringText />
   }
 
   return html ? <RichText /> : <StringText />
 }
 
-const DEFAULT_WIDTH = 100
-const DEFAULT_LINES = 10
-const DEFAULT_HTML = true
+const DEFAULT_WIDTH_VALUE = 100
+const DEFAULT_LINES_VALUE = 3
+const DEFAULT_HTML_VALUE = true
+const DEFAULT_CUSTOM_VALUE = false
 
 export const ControllableShowMore: React.FC<{
   lang: Languages
 }> = ({ lang }) => {
-  const [width, setWidth] = useState(DEFAULT_WIDTH)
-  const [lines, setLines] = useState(DEFAULT_LINES)
-  const [html, setHtml] = useState(DEFAULT_HTML)
+  const [width, setWidth] = useState(DEFAULT_WIDTH_VALUE)
+  const [lines, setLines] = useState(DEFAULT_LINES_VALUE)
+  const [html, setHtml] = useState(DEFAULT_HTML_VALUE)
+  const [custom, setCustom] = useState(DEFAULT_CUSTOM_VALUE)
 
-  const cls = clsx('flex items-center gap-3 my-6')
-  const inputCls = clsx('w-100 max-w-3/4 cursor-pointer')
-  const titleCls = clsx('flex flex-shrink-0 w-18')
+  const isZh = useMemo(() => {
+    return lang === 'zh'
+  }, [])
+
+  const ref = useRef<ShowMoreRef>(null)
+
+  const toggleLines: ShowMoreToggleLinesFn = (e) => {
+    ref.current?.toggleLines(e)
+  }
 
   return (
     <>
-      <div className={cls}>
-        <span className={titleCls}>
-          {getTranslation(lang, 'example.width')}
-        </span>
+      <FormRange
+        lang={lang}
+        labelKey="example.width"
+        value={width}
+        min="50"
+        max="100"
+        defaultValue={DEFAULT_WIDTH_VALUE}
+        onChange={(v) => setWidth(v)}
+      />
 
-        <input
-          className={inputCls}
-          type="range"
-          min="50"
-          max="100"
-          defaultValue={DEFAULT_WIDTH}
-          onChange={(e) => setWidth(Number(e.target.value))}
-        />
+      <FormRange
+        lang={lang}
+        labelKey="example.lines"
+        value={lines}
+        min="1"
+        max="10"
+        defaultValue={DEFAULT_LINES_VALUE}
+        onChange={(v) => setLines(v)}
+      />
 
-        <span>{width}%</span>
-      </div>
+      <FormSwitch
+        lang={lang}
+        labelKey="example.html"
+        checked={html}
+        onChange={(v) => setHtml(v)}
+      />
 
-      <div className={cls}>
-        <span className={titleCls}>
-          {getTranslation(lang, 'example.lines')}
-        </span>
-
-        <input
-          className={inputCls}
-          type="range"
-          min="1"
-          max="10"
-          defaultValue={DEFAULT_LINES}
-          onChange={(e) => setLines(Number(e.target.value))}
-        />
-
-        <span>{lines}</span>
-      </div>
-
-      <div className={cls}>
-        <span className={titleCls}>{getTranslation(lang, 'example.html')}</span>
-
-        <label className="switch">
-          <input
-            type="checkbox"
-            checked={html}
-            onChange={(e) => setHtml(e.target.checked)}
-          />
-          <span className="slider round"></span>
-        </label>
-      </div>
+      <FormSwitch
+        lang={lang}
+        labelKey="example.custom"
+        checked={custom}
+        onChange={(v) => setCustom(v)}
+      />
 
       <ExampleContainer style={{ width: `${width}%` }}>
-        <ShowMore lines={lines} separator={lang === 'zh' ? '' : ' '}>
-          <CurrentContent lang={lang} html={html} />
+        <ShowMore
+          ref={ref}
+          lines={lines}
+          separator={isZh ? '' : ' '}
+          more={
+            custom ? (
+              <CustomButton type="more" isZh={isZh} onClick={toggleLines} />
+            ) : undefined
+          }
+          less={
+            custom ? (
+              <CustomButton type="less" isZh={isZh} onClick={toggleLines} />
+            ) : undefined
+          }
+        >
+          <CurrentContent isZh={isZh} html={html} />
         </ShowMore>
       </ExampleContainer>
     </>
