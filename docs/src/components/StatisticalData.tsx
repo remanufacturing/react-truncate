9ffa35b5b617dc97da87670a654fd9e44b77d157
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-04-28 00:51:03 +0800

    docs: add contributors

--- docs/src/components/StatisticalData.tsx
+++ docs/src/components/StatisticalData.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import pkg from '../../../package.json'
+import type { Languages } from '@/i18n'
+import clsx from 'clsx'
 
 const name = pkg.name
 const repoUrl = pkg.repository.url
-const repo = repoUrl.replace('https://github.com/', '')
+const origin = 'https://github.com/'
+const repo = repoUrl.replace(origin, '')
 const color = '29c1e9'
 
 const options = [
@@ -37,6 +40,87 @@ export const StatisticalData: React.FC = () => {
             rel="sponsored nofollow noopener noreferrer"
           >
             <img alt={i.title} src={i.image} />
+          </a>
+        )
+      })}
+    </div>
+  )
+}
+
+const getContributors = async (cols = 10) => {
+  try {
+    const url = `https://contrib.nn.ci/api?repo=${repo}&cols=${cols}&no_bot`
+    return await fetch(url).then((res) => res.text() || '')
+  } catch (e) {
+    return ''
+  }
+}
+
+const contributorsData = await getContributors()
+
+interface ContributorInfo {
+  homepage: string
+  username: string
+  avatar: string
+}
+
+export const Contributors: React.FC<{
+  lang: Languages
+}> = ({ lang }) => {
+  const getItems = () => {
+    if (typeof window === 'undefined') return []
+
+    const parser = new DOMParser()
+    const xmlDoc = parser.parseFromString(contributorsData, 'text/xml')
+    const svg = xmlDoc.getElementsByTagName('svg')[0]
+    if (!svg) return []
+
+    const elements = xmlDoc.querySelectorAll('a.link')
+    const items = [...elements].map<ContributorInfo>((element) => {
+      const homepage = element?.getAttribute('xlink:href') || ''
+      const username = homepage.replace(origin, '')
+      const avatar = `https://avatars.githubusercontent.com/${username}`
+      return { homepage, username, avatar }
+    })
+
+    return items
+  }
+
+  const items = useMemo(() => {
+    return getItems()
+  }, [])
+
+  const emptyTips = useMemo(() => {
+    return lang === 'zh'
+      ? '获取数据失败，请刷新页面重试。'
+      : 'Failed to obtain data, please refresh the page.'
+  }, [lang])
+
+  const cls = clsx('flex flex-wrap gap-6 w-full mt-6')
+
+  if (!items.length) {
+    return <div className={cls}>{emptyTips}</div>
+  }
+
+  return (
+    <div className={cls}>
+      {items.map((i) => {
+        return (
+          <a
+            key={i.username}
+            title={i.username}
+            href={i.homepage}
+            target="_blank"
+            rel="noreferrer"
+            className="flex flex-col items-center no-underline hover:underline"
+          >
+            <div className="w-16 h-16 rounded-full overflow-hidden">
+              <img
+                className="w-full h-full object-cover"
+                src={i.avatar}
+                alt={i.username}
+              />
+            </div>
           </a>
         )
       })}
