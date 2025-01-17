c29f135e98b4479c886dd7c34feef72f052c2946
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-01-17 23:51:12 +0800

    fix(build): iife bundler corruption

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,6 +6,13 @@ import {
 } from '@bassist/node-utils'
 import pkg from './package.json'
 
+const externalMapping = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+} as const
+
+const external = Object.keys(externalMapping)
+
 export default defineConfig({
   entry: ['src/index.ts'],
   target: ['es2020'],
@@ -20,4 +27,29 @@ export default defineConfig({
   bundle: true,
   minify: true,
   clean: true,
+  esbuildOptions(options) {
+    options.external = external
+  },
+  // https://esbuild.github.io/plugins/#using-plugins
+  esbuildPlugins: [
+    {
+      name: 'external-plugin',
+      setup(build) {
+        build.onResolve(
+          {
+            filter: new RegExp(`^(${external.join('|')})$`),
+          },
+          (args) => ({
+            path: args.path,
+            namespace: 'external',
+          }),
+        )
+
+        build.onLoad({ filter: /.*/, namespace: 'external' }, (args) => {
+          const globalVar = externalMapping[args.path]
+          return { contents: `module.exports=window.${globalVar};` }
+        })
+      },
+    },
+  ],
 })
