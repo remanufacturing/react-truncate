bfc18a3128980c8a179133713722a0fa7dec1cf8
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-01-31 22:28:02 +0800

    feat(build): add remove properties plugin to cleanup test id

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'tsup'
+import { defineConfig, type Options } from 'tsup'
 import {
   defaultBundleFormatConfig,
   getBundleExtension,
   getBundleBanner,
 } from '@bassist/node-utils'
+import { readFile } from 'node:fs/promises'
 import pkg from './package.json'
+
+type ESBuildPlugin = NonNullable<Options['esbuildPlugins']>[number]
 
 const externalMapping = {
   react: 'React',
@@ -12,6 +15,36 @@ const externalMapping = {
 } as const
 
 const external = Object.keys(externalMapping)
+
+const externalPlugin: ESBuildPlugin = {
+  name: 'external-plugin',
+  setup(build) {
+    build.onResolve(
+      { filter: new RegExp(`^(${external.join('|')})$`) },
+      (args) => ({
+        path: args.path,
+        namespace: 'external',
+      }),
+    )
+
+    build.onLoad({ filter: /.*/, namespace: 'external' }, (args) => {
+      const globalVar = externalMapping[args.path]
+      return { contents: `module.exports=window.${globalVar};` }
+    })
+  },
+}
+
+const removePropertiesPlugin: ESBuildPlugin = {
+  name: 'remove-properties-plugin',
+  setup(build) {
+    build.onLoad({ filter: /\.tsx?$/ }, async (args) => {
+      const source = await readFile(args.path, 'utf8')
+      const regex = /\s*(data-testid)\s*=\s*["'][^"']*["']/g
+      const modifiedSource = source.replace(regex, '')
+      return { contents: modifiedSource, loader: 'tsx' }
+    })
+  },
+}
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -31,25 +64,5 @@ export default defineConfig({
     options.external = external
   },
   // https://esbuild.github.io/plugins/#using-plugins
-  esbuildPlugins: [
-    {
-      name: 'external-plugin',
-      setup(build) {
-        build.onResolve(
-          {
-            filter: new RegExp(`^(${external.join('|')})$`),
-          },
-          (args) => ({
-            path: args.path,
-            namespace: 'external',
-          }),
-        )
-
-        build.onLoad({ filter: /.*/, namespace: 'external' }, (args) => {
-          const globalVar = externalMapping[args.path]
-          return { contents: `module.exports=window.${globalVar};` }
-        })
-      },
-    },
-  ],
+  esbuildPlugins: [externalPlugin, removePropertiesPlugin],
 })
